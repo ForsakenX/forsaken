5ebf7b6967052a96ddc93ee0aa6960fc2d3698d8
Daniel Aquino <mr.danielaquino@gmail.com>
2007-09-05 17:03:56 +0000

    Fixed up DIEnumDeviceObjectsProc to not crash the game when joystick is enabled and plugged in...
    git-svn-id: svn://fly.thruhere.net/forsaken@75 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- branches/ProjectX/cleanup/Oct2.c
+++ branches/ProjectX/cleanup/Oct2.c
@@ -5601,152 +5601,177 @@ BOOL FAR PASCAL InitJoystickInput(LPCDIDEVICEINSTANCE pdinst,
    return DIENUM_CONTINUE;
 }
 
+/* this is called for object in our joystick */
+/* objects can be buttons, axis, sliders etc... */
 BOOL CALLBACK DIEnumDeviceObjectsProc( 
-                      LPCDIDEVICEOBJECTINSTANCE lpddoi,
-                      LPVOID pvRef) 
+                      LPCDIDEVICEOBJECTINSTANCE lpddoi, /* the object instance */
+                      LPVOID pvRef) /* pointer to void we passed in from calling block */
 { 
-  GUID   AxisGUID[MAX_JOYSTICK_AXIS - 1];
-  GUID   ButtonGUID = GUID_Button;
-//  GUID   KeyGUID = GUID_Key;
-  GUID   POVGUID = GUID_POV;
-//  GUID   UnknownGUID = GUID_Unknown;
-  GUID   CompareGUID;
-  int *joysticknumptr;
-  int joysticknum, axis;
-  char tempstr[MAX_JOYNAME];
-#if 1
-   AxisGUID[0] = GUID_XAxis;
-   AxisGUID[1] = GUID_YAxis;
-   AxisGUID[2] = GUID_ZAxis;
-   AxisGUID[3] = GUID_RxAxis;
-   AxisGUID[4] = GUID_RyAxis;
-   AxisGUID[5] = GUID_RzAxis;
-   AxisGUID[6] = GUID_Slider; // js can have up to 2 sliders
 
-  joysticknumptr = (int *)(pvRef);
-  joysticknum = *joysticknumptr;
+  int joysticknum, axis, dir;
 
-  CompareGUID = lpddoi->guidType;
+  /* you have to use these or you get a warning */
+  GUID AxisGUID[MAX_JOYSTICK_AXIS - 1];
+  GUID ButtonGUID  = GUID_Button;
+  GUID POVGUID     = GUID_POV;
+  GUID CompareGUID = lpddoi->guidType; /* pointer to the current object GUID type */
+  
+  /* pvRef is a pointer to a passed in object from our calling code */
+  /* here we passed in the current joystick number */
+  joysticknum = *((int *)(pvRef));
 
+  /* types of axis's we have to support */
+  AxisGUID[0] = GUID_XAxis;
+  AxisGUID[1] = GUID_YAxis;
+  AxisGUID[2] = GUID_ZAxis;
+  AxisGUID[3] = GUID_RxAxis;
+  AxisGUID[4] = GUID_RyAxis;
+  AxisGUID[5] = GUID_RzAxis;
+  AxisGUID[6] = GUID_Slider; // js can have up to 2 sliders
+
+  /* loop over the AxisGUID[] Array */
+  /* find out if current object is one of type in AxisGUID[] */
   for (axis = 0; axis < MAX_JOYSTICK_AXIS - 1; axis++)
   {
-    if (IsEqualGuid( &AxisGUID[axis], &CompareGUID ))
-    {
-      if ((axis == AXIS_SliderAxis0) && (JoystickInfo[joysticknum].Axis[AXIS_SliderAxis0].exists))
-        axis++;
 
-      JoystickInfo[joysticknum].Axis[axis].exists = TRUE;
+    /* if the object is not equal to AxisGUID[axis] then continue */
+    if ( ! IsEqualGuid( &AxisGUID[axis], &CompareGUID ))
+      continue;
 
-      if (lpddoi->tszName && *lpddoi->tszName)
-        strncpy( tempstr, lpddoi->tszName, MAX_JOYNAME );
-      else
-        sprintf( tempstr, "Axis %d", axis );
+    /* allocate the memory */
+    JoystickInfo[joysticknum].Axis[axis].name = (char*) malloc (MAX_JOYNAME+1);
+  
+    /* assign all /0's to the stack */
+    memset (JoystickInfo[joysticknum].Axis[axis].name, 0, (MAX_JOYNAME+1));
 
-      JoystickInfo[joysticknum].Axis[axis].name = 
-        (char *) malloc ( strlen ( tempstr ) + 1 );
+    /* if object provides a name */
+    if (lpddoi->tszName && *lpddoi->tszName)
+      /* use the special name */
+      strncpy( JoystickInfo[joysticknum].Axis[axis].name, lpddoi->tszName, MAX_JOYNAME );
+    else
+      /* use generic name */
+      _snprintf( JoystickInfo[joysticknum].Axis[axis].name, MAX_JOYNAME, "Axis %d", axis );
 
-      strcpy ( JoystickInfo[joysticknum].Axis[axis].name, tempstr );
+    /* this current axis exists */
+    JoystickInfo[joysticknum].Axis[axis].exists = TRUE;
 
-      JoystickInfo[joysticknum].Axis[axis].feedback =
-        ( lpddoi->dwType & DIDFT_FFACTUATOR ) ? TRUE : FALSE;
+    /* up the count */
+    /* NumAxis==1 means JoystickInfo[0] is set */
+    JoystickInfo[joysticknum].NumAxis++;
 
-      if ( JoystickInfo[joysticknum].Axis[axis].feedback )
-        DebugPrintf( "Joystick %s Axis %s supports force feedback\n",
-          JoystickInfo[ joysticknum ].Name,
-          JoystickInfo[ joysticknum ].Axis[ axis ].name );
+    /* were done we found out what object we were dealing with */
+    return DIENUM_CONTINUE; 
 
-/*
-      if (lpddoi->tszName && *lpddoi->tszName)
-        strncpy( JoystickInfo[joysticknum].Axis[axis].name, lpddoi->tszName, MAX_JOYNAME );
-      else
-        // no name given back by joystick for axis, therefore assign arbitrary name
-        sprintf( JoystickInfo[joysticknum].Axis[axis].name, "Axis %d", axis );
-*/
-
-      JoystickInfo[joysticknum].NumAxis++;
-      return DIENUM_CONTINUE; 
-    }
   }
 
+  /* if the object is a button */
   if (IsEqualGuid( &ButtonGUID, &CompareGUID ))
   {
 
-#ifdef USE_JOYSTICK_NAMES
-    if (lpddoi->tszName && *lpddoi->tszName)
-      strncpy( tempstr, lpddoi->tszName, MAX_JOYNAME );
-    else
-#endif
-      sprintf( tempstr, "Button %d", JoystickInfo[joysticknum].NumButtons );
-
+    /* allocate the memory */
     JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name = 
-      (char *) malloc ( strlen ( tempstr ) + 1 );
+        (char *) malloc (MAX_JOYNAME+1);
+  
+    /* assign all /0's to the stack */
+    memset (
+       JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name,
+       0,
+       (MAX_JOYNAME+1)
+    );
 
-    strcpy ( JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name, tempstr );
+    /* if object provides a name */
+    if (lpddoi->tszName && *lpddoi->tszName)
+      /* use the special name */
+      strncpy(
+         JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name,
+         lpddoi->tszName,
+         MAX_JOYNAME
+      );
+    else
+      /* use generic name */
+      _snprintf(
+         JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name,
+         MAX_JOYNAME,
+         "Button %d",
+         JoystickInfo[joysticknum].NumButtons
+      );
 
-    JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].feedback_trigger =
-      ( lpddoi->dwType & DIDFT_FFEFFECTTRIGGER ) ? TRUE : FALSE;
-
+    /* up the count */
     JoystickInfo[joysticknum].NumButtons++;
 
-/*    
-    if (lpddoi->tszName && *lpddoi->tszName)
-      strncpy( JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons++].name, lpddoi->tszName, MAX_JOYNAME );
-    else
-    {
-      // no name given back by joystick for button, therefore assign arbitrary name
-      sprintf( JoystickInfo[joysticknum].Button[JoystickInfo[joysticknum].NumButtons].name, "Button %d", JoystickInfo[joysticknum].NumButtons );
-      JoystickInfo[joysticknum].NumButtons++;
-    }
-*/
+    /* were done with this object go to next one */
+    return DIENUM_CONTINUE;
 
-    return DIENUM_CONTINUE; 
   }
 
+  /* if the object is a POV aka Hat Switch */
   if (IsEqualGuid( &POVGUID, &CompareGUID ))
   {
-    int dir;
-
-#ifdef USE_JOYSTICK_NAMES
-    if (lpddoi->tszName && *lpddoi->tszName)
-      strncpy( tempstr, lpddoi->tszName, MAX_JOYNAME );
-    else
-#endif
-      if ( JoystickInfo[ joysticknum ].NumPOVs )
-        sprintf( tempstr, "Hat %d", JoystickInfo[joysticknum].NumPOVs + 1 );
-      else
-        sprintf( tempstr, "Hat" );
-
+    
+    /* allocate the memory */
     JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name = 
-      (char *) malloc ( strlen ( tempstr ) + 1 );
+        (char *) malloc (MAX_JOYNAME+1);
+  
+    /* assign all /0's to the stack */
+    memset (
+       JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name,
+       0,
+       (MAX_JOYNAME+1)
+    );
 
-    strcpy ( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name, tempstr );
+    /* if object provides a name */
+    if (lpddoi->tszName && *lpddoi->tszName)
+      /* use the special name */
+      strncpy(
+         JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name,
+         lpddoi->tszName,
+         MAX_JOYNAME
+      );
+    else
+      /* use generic name */
+      _snprintf(
+         JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name,
+         MAX_JOYNAME,
+         "Hat %d",
+         JoystickInfo[joysticknum].NumPOVs
+      );
 
+    /* set the name of the direction */
     for ( dir = 0; dir < MAX_POV_DIRECTIONS; dir++ )
     {
+
+      /* allocate the memory */
       JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ] = 
-        (char *) malloc ( strlen ( tempstr ) + strlen( JoystickPOVDirections[ dir ] ) + 2 );
-      
-      strcpy ( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ], tempstr );
-      strcat ( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ], " " );
-      strcat ( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ], JoystickPOVDirections[ dir ] );
-    }
+          (char *) malloc (MAX_JOYNAME+1);
+  
+      /* assign all /0's to the stack */
+      memset (
+         JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ],
+         0,
+         (MAX_JOYNAME+1)
+      );
 
+      /* use generic name */
+      _snprintf(
+         JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].dirname[ dir ],
+         MAX_JOYNAME,
+         "%s %d",
+         JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name,
+         JoystickInfo[joysticknum].NumPOVs
+      );
+    }
+  
+    /* up the count */
     JoystickInfo[joysticknum].NumPOVs++;
-/*    
-    if (lpddoi->tszName && *lpddoi->tszName)
-      strncpy( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs++].name, lpddoi->tszName, MAX_JOYNAME );
-    else
-    {
-      // no name given back by joystick for POV, therefore assign arbitrary name
-      sprintf( JoystickInfo[joysticknum].POV[JoystickInfo[joysticknum].NumPOVs].name, "POV %d", JoystickInfo[joysticknum].NumPOVs );
-      JoystickInfo[joysticknum].NumPOVs++;
-    }
-*/
 
+    /* were done with this object go to next */
     return DIENUM_CONTINUE; 
+
   }
-#endif
+
+  /* default we couldn't find out what this object is so continue */
   return DIENUM_CONTINUE; 
+
 }
 
 
@@ -6395,21 +6420,21 @@ void ProcessGameKeys( void )
 #ifndef POLYGONAL_COLLISIONS
 #ifdef REMOTE_CAMERA_ENABLED
 
-	// F12
-	if ( IsKeyPressed( DIK_F12 ) )
-	  if( !RemoteCameraActive )
-	  {
-		RemoteCameraActive = TRUE;
-		Ships[MAX_PLAYERS].Object.Pos = Ships[WhoIAm].Object.Pos;
-		Ships[MAX_PLAYERS].Object.Group = Ships[WhoIAm].Object.Group;
-		Ships[MAX_PLAYERS].Object.Mode = NORMAL_MODE;
-		Ships[MAX_PLAYERS].enable = 1;
-		Current_Camera_View = MAX_PLAYERS;    // which object is currently using the camera view....
-	  }else{
-		RemoteCameraActive = FALSE;
-		Ships[MAX_PLAYERS].enable = 0;
-		Current_Camera_View = WhoIAm;   // which object is currently using the camera view....
-	  }
+  // F12
+  if ( IsKeyPressed( DIK_F12 ) )
+    if( !RemoteCameraActive )
+    {
+    RemoteCameraActive = TRUE;
+    Ships[MAX_PLAYERS].Object.Pos = Ships[WhoIAm].Object.Pos;
+    Ships[MAX_PLAYERS].Object.Group = Ships[WhoIAm].Object.Group;
+    Ships[MAX_PLAYERS].Object.Mode = NORMAL_MODE;
+    Ships[MAX_PLAYERS].enable = 1;
+    Current_Camera_View = MAX_PLAYERS;    // which object is currently using the camera view....
+    }else{
+    RemoteCameraActive = FALSE;
+    Ships[MAX_PLAYERS].enable = 0;
+    Current_Camera_View = WhoIAm;   // which object is currently using the camera view....
+    }
 
 #endif
 #endif
@@ -6881,6 +6906,8 @@ RenderScene(LPDIRECT3DDEVICE Null1, LPDIRECT3DVIEWPORT Null2 )
     LeaveCriticalSection (&SfxKey);
   }
 
+// This is where in game we are getting input data read
+// Why would it be wrapped in this def check ?
 #ifndef MULTIPLE_READINPUTS
   ReadInput();
 #endif
