1f2705131345bea575cd4f503e75da872d290770
Daniel Aquino <mr.danielaquino@gmail.com>
2009-03-07 22:42:22 +0000

    Removed unneeded code.
    git-svn-id: svn://fly.thruhere.net/forsaken@636 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/Include/Local.h
+++ ProjectX/Include/Local.h
@@ -1,16 +1,1 @@
-
-// localize language
-#if defined ( VERSION_SPANISH )
-#include "SpanishLocal.h"
-#elif defined( VERSION_ITALIAN )
-#include "ItalianLocal.h"
-#elif defined( VERSION_GERMAN )
-#include "GermanLocal.h"
-#elif defined( VERSION_FRENCH )
-#include "FrenchLocal.h"
-#else
 #include "EnglishLocal.h"
-#endif
-
-
-
