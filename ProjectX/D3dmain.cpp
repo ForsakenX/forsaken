93ebe98b5f21b95ff37a8fd46f865d7876fe36ce
Daniel Aquino <mr.danielaquino@gmail.com>
2009-05-08 08:42:05 +0000

    created a function to convert between wide char's and narrow char's... wrote a breakpad call back that attempts to launch Utils/crash_reporter.exe... falls back to a MessageBox telling user to email the dump... if breakpad fails to create the dump then error handling is not blocked allowing the system debugger to catch the error... Quick change of Forsaken to ProjectX in MessageBox Title's...
    git-svn-id: svn://fly.thruhere.net/forsaken@980 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/D3dmain.cpp
+++ ProjectX/D3dmain.cpp
@@ -514,15 +514,68 @@ BOOL parse_chdir( char *cli )
 	return TRUE;
 }
 
-static BOOL init_breakpad( void )
+static void wchar_to_char( char * dst_ptr, const wchar_t * src_ptr )
+{
+	while( *src_ptr != 0 ) // while not end of source
+		*dst_ptr++ = (char) *src_ptr++; // conver to char
+	*dst_ptr = 0; // finish string
+}
+
+static bool breakpad_callback(
+	const wchar_t* dump_path,
+	const wchar_t* minidump_id,
+	void* context,
+	EXCEPTION_POINTERS* exinfo,
+	MDRawAssertionInfo* assertion,
+	bool succeeded 
+)
+{
+	// if writing to dump file succeeded
+	if(succeeded)
+	{    
+		STARTUPINFO si;
+		PROCESS_INFORMATION pi;
+
+		char path[256];
+		char dir[256];
+		char id[256];
+
+		wchar_to_char( &dir[0], &dump_path[0] );
+		wchar_to_char( &id[0],  &minidump_id[0] );
+
+		sprintf(path, "%s\\%s.dmp", dir, id);
+		
+		ZeroMemory( &si, sizeof(si) );
+		ZeroMemory( &pi, sizeof(pi) );
+		si.cb = sizeof(si);
+
+		if(!CreateProcess( "Utils/crash_reporter.exe", path,
+							NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi ))
+		{
+			Msg("%s\n%s\n%s\n%s",
+				"Error launching crash reporting tool!",
+				"Crash report saved to: ", path,
+				"Please email it to fskn.methods@gmail.com");
+		}
+	}
+
+	// if we succeeded creating a dump
+	// then stop any other handlers from running
+	return succeeded;
+}
+
+static BOOL breakpad_init( void )
 {
 	using namespace google_breakpad;
 	static ExceptionHandler * e;
-	wstring path(L"./Dumps");
-	e = new ExceptionHandler( path, NULL, NULL, NULL, ExceptionHandler::HANDLER_ALL );//, MiniDumpNormal, pipe_name, custom_info );
-	// TODO: 
-	//	 1: out of process dumps ?
-	//   2: use callback function to launch crash reporter application
+	wstring path(L"Dumps");
+	e = new ExceptionHandler(
+		path, 
+		NULL, 
+		(google_breakpad::ExceptionHandler::MinidumpCallback) &breakpad_callback, 
+		NULL, 
+		ExceptionHandler::HANDLER_ALL 
+	);//, MiniDumpNormal, pipe_name, custom_info );
 	DebugPrintf("initialized breakpad\n");
 	return TRUE;
 }
@@ -539,7 +592,7 @@ static BOOL AppInit(HINSTANCE hInstance, LPSTR lpCmdLine)
 	LastTime = LargeTime;
 
 	// initialize google breakpad crash reporting
-	if(!init_breakpad())
+	if(!breakpad_init())
 		return FALSE;
 
 	// test breakpad by uncommenting this
@@ -1380,7 +1433,7 @@ Msg( LPSTR fmt, ... )
     if (d3dapp && d3dapp->bFullscreen)
         SetWindowPos(myglobs.hWndMain, HWND_NOTOPMOST, 0, 0, 0, 0,
                      SWP_NOSIZE | SWP_NOMOVE);
-    MessageBox( NULL, buff, "Forsaken", MB_OK );
+    MessageBox( NULL, buff, "ProjectX", MB_OK );
     if (d3dapp && d3dapp->bFullscreen)
         SetWindowPos(myglobs.hWndMain, HWND_TOPMOST, 0, 0, 0, 0,
                      SWP_NOSIZE | SWP_NOMOVE);
