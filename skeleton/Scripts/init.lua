6ff23e05add05d76086c6f5c9eefd6fa7c86291e
Daniel Aquino <mr.danielaquino@gmail.com>
2009-07-04 18:11:38 +0000

    Setup init.lua more properly and required the new games module.
    git-svn-id: svn://fly.thruhere.net/forsaken@1060 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- skeleton/Scripts/init.lua
+++ skeleton/Scripts/init.lua
@@ -1,7 +1,4 @@
 
--- main configuration table
-config = {}
-
 -- dofile that looks in proper folder
 function dofile(filename)
 	path="./Scripts/"..filename
@@ -15,10 +12,15 @@ function init( debug )
 	-- tell require() to look in ./Scripts/*
 	package.path = "./Scripts/?.lua;" .. package.path
 
+	-- globals
+	config = {} -- main config table
+
+	-- load modules
+	games = require("games")
+
 	-- load library files
         dofile("extensions.lua")
         dofile("helpers.lua")
-	dofile("games.lua")
         dofile("config.lua")
 
 	-- load config
