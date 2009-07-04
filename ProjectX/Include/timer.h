bc842ef9147db6c63ffae48028e9258656edd9dc
Daniel Aquino <mr.danielaquino@gmail.com>
2009-07-04 21:44:55 +0000

    Added a how to to the timer and timer_peek which doesn't modify the timer object.
    git-svn-id: svn://fly.thruhere.net/forsaken@1065 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/Include/timer.h
+++ ProjectX/Include/timer.h
@@ -1,6 +1,25 @@
 #ifndef TIMER
 #define TIMER
 
+/*
+	setup:
+
+			timer t;
+			timer_clear( & t );
+
+	time passed between two points:
+
+			timer_run( &t );
+			sleep(10);
+			duration = timer_run( &t );
+
+	peeking at the time without modifing last time:
+
+			duration = timer_peek( &t );
+
+*/
+
+
 // pukes
 #include <Windows.h>
 
@@ -14,6 +33,7 @@ typedef struct timer {
 } timer;
 
 float timer_run   ( timer* );
+float timer_peek  ( timer* );
 void  timer_clear ( timer* );
 void  timer_debug ( char*, timer* );
 
