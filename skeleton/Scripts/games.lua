139358fb01cc8b8678b0e575417ede84d0ea5eef
Daniel Aquino <mr.danielaquino@gmail.com>
2009-07-04 18:10:12 +0000

    Added simple functions to make it easy to create c helpers to iterate game list.
    git-svn-id: svn://fly.thruhere.net/forsaken@1059 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- skeleton/Scripts/games.lua
+++ skeleton/Scripts/games.lua
@@ -1,15 +1,60 @@
 
-local http = require("socket.http")
+local base 	= _G
+local os 	= require("os")
+local table 	= require("table")
+local http 	= require("socket.http")
+local assert	= assert
+local pcall	= pcall
+local loadstring	= loadstring
 
-function games( url )
+module("games")
+
+local url = "http://fly.thruhere.net/status/games.json"
+
+function get( url )
 	local list = {}
-	local url = "http://fly.thruhere.net/status/games.json"
 	local body, code, headers = http.request(url)
 	if code ~= 200 then return list end
 	body = "return " .. body
-	local code = assert(loadstring(body))
-	if not code then return list end
-	list = code()
+	local rv,f = pcall(loadstring, body)
+	if not (rv and f) then return list end
+	list = f()
 	return list
 end
+
+-----------------------------------------------------------------------------
+-- C Helpers
+----------------------------------------------------------------------------
+
+local list = {}
+local last_time = 0
+local refresh_time = 1 -- seconds
+
+function update()
+	now = os.time()
+	if ( now - last_time >= refresh_time ) then
+		list = get( url )
+	end
+end
+
+function length()
+	update()
+	return table.maxn(list)
+end
+
+function name_at( index )
+	update()
+	return list[index].nick
+end
+
+function ip_at( index )
+	update()
+	return list[index].ip
+end
+
+function port_at( index )
+	update()
+	return list[index].port
+end
+
 
