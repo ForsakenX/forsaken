2baceda0afb5286d497d3afb4ed8b735a5c15773
Daniel Aquino <mr.danielaquino@gmail.com>
2009-10-07 04:52:42 +0000

    moving folders out of branches to reduce level of nesting
    git-svn-id: svn://fly.thruhere.net/forsaken@1206 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- branches/ProjectX/d3d9/singleplayer.h
+++
@@ -1,7 +0,0 @@
-#ifndef SINGLEPLAYER_INCLUDED
-#define SINGLEPLAYER_INCLUDED
-
-BOOL LoadASinglePlayerGame( MENUITEM * Item );
-BOOL StartASinglePlayerGame( MENUITEM * Item );
-
-#endif	// SINGLEPLAYER_INCLUDED
