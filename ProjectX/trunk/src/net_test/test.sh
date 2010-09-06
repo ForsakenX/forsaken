fef6dbd1ae49706dbee4d7f2bcd07b8bbbdbbe82
Daniel Aquino <mr.danielaquino@gmail.com>
2010-09-06 03:34:11 +0000

    changed test script to drop you into one of the clients so you can send messages
    git-svn-id: svn://fly.thruhere.net/forsaken@1854 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/trunk/src/net_test/test.sh
+++ ProjectX/trunk/src/net_test/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 trap 'killall -9 client; rm log.*' SIGINT
 ./client server &> log.server &
-for ((x=1; x<=40; x++)); do
+for ((x=1; x<40; x++)); do
 	port="2`printf "%-.4d" $x`"
 	player="player${port}"
 	./client $player $port localhost 2300 &> log.$port &
 done
+./client player0 20000 localhost 2300 | tee log.20000
 wait
