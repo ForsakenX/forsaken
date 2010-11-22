2130a6595591306c5889dda6ab0f9ea71edf157a
Daniel Aquino <mr.danielaquino@gmail.com>
2010-11-22 01:13:40 +0000

    updating notes for bot file
    git-svn-id: svn://fly.thruhere.net/forsaken@2035 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/trunk/src/bot1.c
+++ ProjectX/trunk/src/bot1.c
@@ -38,9 +38,10 @@
 extern GLOBALSHIP Ships[MAX_PLAYERS+1];
 extern BYTE WhoIAm;
 /*
+
 	RenderScene()
 	{
-		ReadInput()
+		ReadInput(); // key/mouse read here
 		switch(STATUS)
 		{
 		case STATUS_Normal:
@@ -49,81 +50,93 @@ extern BYTE WhoIAm;
 				MainRoutines()
 				{
 					ProcessBot(); // you are here
-					...           // reset of processing goes here
+					ProcessShips()
+					{
+						ModeControl()
+						{
+							control_ship()
+							{
+								// applies key/mouse inputs against
+								// player config to perform actions
+							}
+						}
+					}
+					// rest of game processing goes here
 				}
 			}
 		break;
 		}
 	}
+
+ Ships[]
+	.Object
+	.enable
+	.ShipThatLastHitMe
+	.NumMultiples
+	.StealthTime
+	.InvulTimer
+	.Invul
+	.LastAngle
+	.Damage
+	.Primary
+	.Secondary
+	.Pickups
+	.Mines
+	.LastMove
+	.Move_Off
+	.OrbAmmo[]
+	.PrimPowerLevel
+	.PrimID
+	.SecID
+	.SecWeapon
+	.headlights
+	.OldPos
+	.NextPos
+	.OldQuat
+	.NextQuat
+	.OldBank
+	.NextBank
+	.SuperNashramTimer
+	.ShieldHullCount
+	.RealPos
+	.RealGroup
+
+ Pickups[ MAXPICKUPTYPES ]
+	NextInGroup
+	PrevInGroup
+	Pos
+	Group
+	Owner
+	Type == PICKUP_* (ex: PICKUP_Trojax)
+	Mode == PICKUPMODE_Normal
+
+ BOOL PickupsGot[ MAXPICKUPSTYPES ] 
+	PICKUP_* (ex: PICKUP_Trojax)
+
+ Mloadheader // level
+	->CellSize
+	->Group[]
+		.name
+		.center
+		.half_size
+		.up
+		.num_portals
+		.*Portal
+			.visible
+			.Verts
+		.cell_origin[]
+		.xcells[]
+		.ycells[]
+		.zcells[]
+		.numofcells[]
+		.StartPosInThisGroup
+
 */
 void ProcessBot1( void )
 {
-	// Ships[]
-	//	.Object
-	//	.enable
-	//	.ShipThatLastHitMe
-	//	.NumMultiples
-	//	.StealthTime
-	//	.InvulTimer
-	//	.Invul
-	//	.LastAngle
-	//	.Damage
-	//	.Primary
-	//	.Secondary
-	//	.Pickups
-	//	.Mines
-	//	.LastMove
-	//	.Move_Off
-	//	.OrbAmmo[]
-	//	.PrimPowerLevel
-	//	.PrimID
-	//	.SecID
-	//	.SecWeapon
-	//	.headlights
-	//	.OldPos
-	//	.NextPos
-	//	.OldQuat
-	//	.NextQuat
-	//	.OldBank
-	//	.NextBank
-	//	.SuperNashramTimer
-	//	.ShieldHullCount
-	//	.RealPos
-	//	.RealGroup
-	//
-	// Pickups[ MAXPICKUPTYPES ]
-	//	NextInGroup
-	//	PrevInGroup
-	//	Pos
-	//	Group
-	//	Owner
-	//	Type == PICKUP_* (ex: PICKUP_Trojax)
-	//	Mode == PICKUPMODE_Normal
-	//
-	// BOOL PickupsGot[ MAXPICKUPSTYPES ] 
-	//	PICKUP_* (ex: PICKUP_Trojax)
-	//
-	// Mloadheader // level
-	//	->CellSize
-	//	->Group[]
-	//		.name
-	//		.center
-	//		.half_size
-	//		.up
-	//		.num_portals
-	//		.*Portal
-	//			.visible
-	//			.Verts
-	//		.cell_origin[]
-	//		.xcells[]
-	//		.ycells[]
-	//		.zcells[]
-	//		.numofcells[]
-	//		.StartPosInThisGroup
-	//
-// your ship
+	// your ship
 	GLOBALSHIP * me = &Ships[WhoIAm];
-// move your own ship
+	// move your own ship
 	me->Object.Speed.z = 1;
 }
 #endif // BOT1
