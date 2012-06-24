91f1b2727ba6c32b5379a72755349df64cf56394
Daniel Aquino <mr.danielaquino@gmail.com>
2012-06-24 06:16:15 +0000

    adding header define for DebugPuts
    git-svn-id: svn://fly.thruhere.net/forsaken@2324 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- ProjectX/trunk/src/util.h
+++ ProjectX/trunk/src/util.h
@@ -14,6 +14,7 @@ int Msg( const char * fmt, ... );
 void DebugPrintf_( const char * format, ... ); // no prefix timestamp
 void DebugPrintf( const char * format, ... );  // prefix timestamp
 void DebugState( const char * str );           // only prints if str changed
+void DebugPuts( char * str );                  // 
 
 void Change_Ext( const char * Src, char * Dest, const char * Ext );
 void Get_Ext( uint8 * Src, uint8 * Dest );
