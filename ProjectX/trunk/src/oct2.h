2e51d326ec77ebeb76e8a652e36cd3eebc4004ed
Daniel Aquino <mr.danielaquino@gmail.com>
2012-11-04 16:52:54 +0000

    forgot to svn-add oct2.h earlier
    git-svn-id: svn://fly.thruhere.net/forsaken@2467 1361c5d3-c82f-4d94-b015-8aedb5f389d9

---
+++ ProjectX/trunk/src/oct2.h
@@ -0,0 +1,11 @@
+#ifndef OCT2_INCLUDED
+#define OCT2_INCLUDED
+
+#include "main.h"
+
+#define MAX_SHORT_LEVEL_NAME 32
+extern char ShortLevelNames[MAXLEVELS][MAX_SHORT_LEVEL_NAME];
+
+extern int16_t NewLevelNum;
+
+#endif // OCT2_INCLUDED
