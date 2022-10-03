6ca5497cfbd7edfaf1c2e9af5eadda0a5ef606ba
Daniel Aquino <mr.danielaquino@gmail.com>
2022-10-02 20:21:10 -0400

    update version.h to support easy build flags

--- version.h
+++ version.h
@@ -1,49 +1,78 @@
 #ifndef VERSION_HEADER
 #define VERSION_HEADER
 
-// major version
-#define PXV	 "1"
+//
+// These are some helpers that make us able to concat values as desired
+//
 
-// multiplayer version (increase if you break multiplayer compatibility)
-#define PXMPV	 "18"
+// you need 2 levels of indirection to make things work right
+// https://stackoverflow.com/questions/1489932/how-can-i-concatenate-twice-with-the-c-preprocessor-and-expand-a-macro-as-in-ar
 
-// multiplayer compatibility flag
-		// TODO: use this format in future for now hard coded to existing format
-		//#define PXMPVINT PXV.PXMPV
-#define PXMPVINT 118
+// combine two raw snippets into one
+#define CONCAT2(a,b) a ## b
+#define CONCAT(a,b) CONCAT2(a,b)
 
-// revision (should be provided at build time for official builds)
-// make PXRV=$(svn info | grep Revision | awk '{print $NF}')
-#ifndef PXRV
-    #define PXRV	 "0"
+// wraps a value as a string
+#define STR2(x) #x
+#define STR(x) STR2(x)
+
+
+
+//
+// Version information
+//
+//
+// The basic versioning premis is:
+//
+//   major.network.build-platform-arch
+//
+//   major     = major version
+//   network   = network compatability (bump on breaking changes)
+//   build     = incremental build version
+//   platform  = windows/linux/mac/etc.
+//   arch      = 32/64bit
+//
+
+#ifndef VERSION_MAJOR
+	#define VERSION_MAJOR 1
 #endif
 
-// tag name so we know what type of build this is
-#ifndef PXBV
-    #ifdef WIN32
-        #ifdef D3D
-            #define PXBV  "D3D9"
-        #else
-            #define PXBV  "OPEN"
-        #endif
+#ifndef VERSION_NETWORK
+	#define VERSION_NETWORK	18
+#endif
+
+#ifndef VERSION_BUILD
+    #define VERSION_BUILD 0
+#endif
+
+#ifndef VERSION_PLATFORM
+    #if defined(WIN32)
+        #define VERSION_PLATFORM  "WINDOWS"
     #elif defined(MACOSX)
-        #define PXBV  "MACOSX"
+        #define VERSION_PLATFORM  "MACOSX"
     #else
-        #define PXBV  "LINUX"
+        #define VERSION_PLATFORM  "LINUX"
     #endif
 #endif
 
-// architecture
 #ifdef __i386__
-  #define ARCH "32"
+	#define ARCH "32"
 #else
-  #define ARCH "64"
+	#define ARCH "64"
 #endif
 
-// the full version name (must contain no spaces for net_tracker)
-#define PXVersion "ProjectX-" PXV "." PXMPV "." PXRV "-" PXBV "-" ARCH
+// the full stringified version name
+// must contain no spaces for net_tracker
+#define PX_VERSION_STR \
+		"ProjectX-" \
+		STR(VERSION_MAJOR) "." \
+		STR(VERSION_NETWORK) "." \
+		STR(VERSION_BUILD) "-" \
+		VERSION_PLATFORM \
+		"-" \
+		ARCH
 
-// this should be plenty if not please increase this
-#define MAX_PXVersion 45
+// the multiplayer version as an integer used for network packets
+#define PX_VERSION_NETWORK_COMPAT_INT CONCAT(VERSION_MAJOR, VERSION_NETWORK)
 
 #endif
